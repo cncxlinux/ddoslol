331203ae6a0bc32b13f2c2f0fa53c70ae52bd83f
cncxlinux <64398814+cncxlinux@users.noreply.github.com>
2020-04-27 13:54:08 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import time
+
+xlinux = """
+DDOS AMPUH 100%
+BY. xlinux
+"""
+# Tapi Tipu awoaowkwoakakwokwkkw
+print(xlinux)
+start1 =input("Masukan Web nya :") 
+start2 =input("Masukan IP Web nya :") 
+start3 =input("Masukan Port :") 
+start4 =input("Masukan Packet :") 
+print("Website :", start1) 
+print("IP Website :", start2)
+print("Port Website :", start3)
+print("Packet Yg Di Kirim :", start4)
+print()
+
+#:)
+while start4:
+    print('sedang di Attack......')
+    time.sleep(0.20)
+    
+
+    
+   
+    
+
+   
+
+
+    
+
+
+
+
+
+
